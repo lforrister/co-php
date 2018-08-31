1fb219c90be26904883bab26c65a2b262fdb9606
Lindsay Forrister <lindsayforrister@gmail.com>
2018-08-31 09:36:55 -0500

    Upload

---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html >
+<head>
+
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.7.7, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo-square.png" type="image/x-icon">
+  <meta name="description" content="">
+  <title>Home</title>
+  <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/assets/tether/tether.min.css">
+  <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/assets/animatecss/animate.min.css">
+  <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/assets/dropdown/css/dropdown-style.css">
+  <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/assets/theme/css/theme-style.css">
+  <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/assets/mobirise/css/mbr-additional.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/assets/LFstyle.css" type="text/css">
+
+
+  
+  
+  
+</head>
+<body>
+  <section class="menu menu-section" once="menu" id="menu1-0">
+
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm bg-color transparent">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="http://cortezoil.com">
+                         <img src="<?php echo get_bloginfo('template_directory'); ?>/assets/images/cortez1-404x131.png" alt="Cortez Resources" title="" style="height:6rem;" class="logo-white">
+                         
+                    </a>
+                </span>
+                <span class="navbar-logo">
+                    <a href="http://cortezoil.com" >
+<img src="<?php echo get_bloginfo('template_directory'); ?>/assets/images/cortezlogo-white.png" alt="Cortez Resources" title="" style="height: 6rem;" class="logo-white2">
+                    </a>
+                </span>
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true">
+                    <li class="nav-item"><a class="nav-link link text-info display-4 home-menu" href="http://cortezoil.com" aria-expanded="false">HOME</a></li>
+                    <li class="nav-item"><a class="nav-link link text-info display-4 home-menu" href="<?php echo get_bloginfo('template_directory'); ?>/about.html" aria-expanded="false">ABOUT</a></li>
+                    <li class="nav-item"><a class="nav-link link text-info display-4 home-menu" href="<?php echo get_bloginfo('template_directory'); ?>/our-team.html" aria-expanded="false">OUR TEAM</a></li>
+                    <li class="nav-item"><a class="nav-link link text-info display-4 home-menu" href="<?php echo get_bloginfo('template_directory'); ?>/contact.html" aria-expanded="false">CONTACT US</a></li>
+                
+            
+        </div>
+    </nav>
+</section>
+
+<section class="engine"></section><section class="cid-qXUPi99FrN mbr-fullscreen mbr-parallax-background" id="header2-1y">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.4; background-color: rgb(191, 29, 33);"></div>
+
+    <div class="container-fluid align-center">
+        <div class="row">
+            <div class="mbr-white header-block">
+                <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1"><span style="font-weight: normal;">
+                    WELCOME TO</span> CORTEZ</h1>
+                
+                <p class="mbr-text pb-3 mbr-fonts-style display-5" style="font-weight:300;">
+                    Cortez Resources is a private oil and gas exploration and production company focused on prospect generation and leasehold, mineral, and royalty acquisitions. Our assets span over 700,000 gross acres across the United States, under multiple active basins, and we currently have interests in over a thousand wells. We are always looking to grow our portfolio and are currently targeting assets in the Permian basin and Gulf Coast region,  as well as other unconventional plays in the lower 48 United States.
+                </p>
+                <div class="mbr-section-btn"><a class="btn btn-md btn-secondary display-4" href="<?php echo get_bloginfo('template_directory'); ?>/contact.html">SEND US A DEAL</a>
+                    <a class="btn btn-md btn-success display-4" href="<?php echo get_bloginfo('template_directory'); ?>/contact.html">REQUEST AN OFFER</a></div>
+            </div>
+        </div>
+    </div>
+    
+</section>
+
+<section class="mbr-section content4 cid-qXIjHf0v6A" id="content4-15">
+
+
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="title col-12 col-md-8">
+                <h2 class="align-center pb-3 mbr-fonts-style display-1" id="our-plays">
+                    OUR
+                    <strong>PLAYS</strong>
+                </h2>
+
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="map2 map-section" id="map2-5">
+
+    
+
+    <div class="container-fluid" id="container-map">
+            
+               <img class="map-img" style="display:block; margin-left:auto; margin-right:auto;" src="<?php echo get_bloginfo('template_directory'); ?>/assets/images/map-large2.png">
+      
+</div>
+
+</section>
+
+<section class="cid-qXVbIbSDiZ" id="footer5-2l">
+
+
+
+
+
+    <div class="container">
+        <div class="media-container-row" style="padding-bottom: 0px;">
+            <div class="col-md-3">
+                <div class="media-wrap" style="padding-top:0px;">
+                        <a href="http://cortezoil.com">
+                        <img src="<?php echo get_bloginfo('template_directory'); ?>/assets/images/cortezlogo-white.png" alt="Cortez Resources" title="">
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-9">
+                <p class="mbr-text align-right links mbr-fonts-style display-7" style="line-height:6;">
+                    <a href="http://cortezoil.com" class="text-white">HOME</a> &nbsp;&nbsp;&nbsp;&nbsp;
+                    <a href="<?php echo get_bloginfo('template_directory'); ?>/about.html" class="text-white">ABOUT</a> &nbsp;&nbsp;&nbsp;&nbsp;
+                    <a href="<?php echo get_bloginfo('template_directory'); ?>/our-team.html" class="text-white">TEAM</a> &nbsp;&nbsp;&nbsp;&nbsp;
+                    <a href="<?php echo get_bloginfo('template_directory'); ?>/contact.html" class="text-white">CONTACT</a>
+                </p>
+                <br>
+            </div>
+        </div>
+        <div class="media-container-row">
+            <p> <a style="color:white; font-size:11px; margin-top: -5%;" href="http://lindsayforrister.com" target="blank"> Web Design/Development by Lindsay Forrister </a> </p>
+        </div>
+
+
+    </div>
+    </div>
+</section>
+
+
+  <script src="<?php echo get_bloginfo('template_directory'); ?>/assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="<?php echo get_bloginfo('template_directory'); ?>/assets/popper/popper.min.js"></script>
+  <script src="<?php echo get_bloginfo('template_directory'); ?>/assets/tether/tether.min.js"></script>
+  <script src="<?php echo get_bloginfo('template_directory'); ?>/assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="<?php echo get_bloginfo('template_directory'); ?>/assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="<?php echo get_bloginfo('template_directory'); ?>/assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="<?php echo get_bloginfo('template_directory'); ?>/assets/viewportchecker/jquery.viewportchecker.js"></script>
+  <script src="<?php echo get_bloginfo('template_directory'); ?>/assets/parallax/jarallax.min.js"></script>
+  <script src="<?php echo get_bloginfo('template_directory'); ?>/assets/dropdown/js/script.min.js"></script>
+  <script src="<?php echo get_bloginfo('template_directory'); ?>/assets/theme/js/script.js"></script>
+  
+  
+  <input name="animation" type="hidden">
+  </body>
+</html>
